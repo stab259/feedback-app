185af84d8fa0a520451c1f69c41bbd57a80be999
stab259 <nhientran2509@gmail.com>
2022-02-15 15:25:22 +0700

    Creating Routes and Links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 import FeedbackData from "./data/FeedbackData"
 
 function App() {
@@ -19,21 +22,32 @@ function App() {
         console.log(newFeedback)
     }
     return (
-        <>
+        <Router>
             <Header />
             <div className="container">
-                <FeedbackForm
-                    handleAdd={addFeedback}
-                />
-                <FeedbackStats
-                    feedback={feedback}
-                />
-                <FeedbackList
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                />
+                <Routes>
+                    <Route exact path='/'
+                        element={
+                            <>
+                                <FeedbackForm
+                                    handleAdd={addFeedback}
+                                />
+                                <FeedbackStats
+                                    feedback={feedback}
+                                />
+                                <FeedbackList
+                                    feedback={feedback}
+                                    handleDelete={deleteFeedback}
+                                />
+                            </>
+                        }
+                    >
+                    </Route>
+                    <Route path='/about' element={<AboutPage />}></Route>
+                </Routes>
+                <AboutIconLink />
             </div>
-        </>
+        </Router >
     )
 }
 
